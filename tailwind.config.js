aed583d9029d1bf391101a72d1f774f8951cd131
A-B620 <abdullahhameed166@gmail.com>
2024-05-08 06:54:18 -0700

    adding

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    // Your theme configuration
+  },
+  plugins: [
+    // Your plugin configuration
+  ],
+}
+
